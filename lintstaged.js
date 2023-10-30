3a3e2b777740eed92f6c8830d9df6126f2b3b5ab
Stepan Tytseiko <stitseyko@mail.ru>
2023-10-31 00:16:12 +0200

    added test changes

--- lintstaged.js
+++ lintstaged.js
@@ -1,8 +1,10 @@
 module.exports = {
     '*.{js,jsx,ts,tsx}': [
-        'eslint --max-warnings=0', 
+        'eslint --max-warnings=0',
         'react-scripts test --bail --watchAll=false --findRelatedTests --passWithNoTests',
         () => 'tsc-files --noEmit',
     ],
-    '*.{js,jsx,ts,tsx,json,css,js}': ['prettier --write --ignore-path .gitignore .'],
+    '*.{js,jsx,ts,tsx,json,css,js}': [
+        'prettier --write --ignore-path .gitignore .',
+    ],
 }
